e4985d9aad689f629f5a96f57fd0e8177084bb07
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-28 12:20:37 +0300

    Add getConnectedDevices.js to get info about connected media devices

---
+++ getConnectedDevices.js
@@ -0,0 +1,29 @@
+//Get 'audioinput'/'audiooutput'/'videoinput' kind devices
+const getConnectedDevices = async (type) => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  return devices.filter((device = device.kind === type));
+};
+
+// Get video cameras
+try {
+  const cameras = getConnectedDevices("videoinput");
+  console.log("Cameras:", cameras);
+} catch (err) {
+  console.error("Error:", err);
+}
+
+// Get audio inputs
+try {
+  const microphones = getConnectedDevices("audioinput");
+  console.log("Microphones:", microphones);
+} catch (err) {
+  console.error("Error:", err);
+}
+
+// Get audio outputs
+try {
+  const speakers = getConnectedDevices("audiooutput");
+  console.log("Speakers:", speakers);
+} catch (err) {
+  console.error("Error:", err);
+}
